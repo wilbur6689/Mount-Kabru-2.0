19f25eb73f2c4437c2d828d157cd587db13c6147
Phillip Willis <wilbur6689@gmail.com>
2017-09-22 09:56:46 -0400

    getting things setup

---
+++ src/main/java/com/game/Mount/Kabru/MountKabruApplication.java
@@ -0,0 +1,12 @@
+package com.game.Mount.Kabru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountKabruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountKabruApplication.class, args);
+	}
+}
